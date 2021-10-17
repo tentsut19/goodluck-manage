ea271e6223370113426a4d5094c6b38a79e635e1
2200224 <thanarat.s@cimbthai.com>
2021-10-17 21:59:21 +0700

    .

--- src/main/java/th/co/infinitait/goodluck/config/SpringAsyncConfiguration.java
+++ src/main/java/th/co/infinitait/goodluck/config/SpringAsyncConfiguration.java
@@ -2,17 +2,25 @@ package th.co.infinitait.goodluck.config;
 
 
 import java.lang.reflect.Method;
+import java.util.Date;
 import java.util.concurrent.Executor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import th.co.infinitait.goodluck.entity.UpdateOrderEntity;
+import th.co.infinitait.goodluck.repository.UpdateOrderRepository;
 
 @Slf4j
 @Configuration
 public class SpringAsyncConfiguration implements AsyncConfigurer {
+
+    @Autowired
+    private UpdateOrderRepository updateOrderRepository;
+
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
@@ -31,6 +39,24 @@ public class SpringAsyncConfiguration implements AsyncConfigurer {
             public void handleUncaughtException(Throwable ex, Method method, Object... params) {
                 log.info("Throwable Exception message : {}", ex.getMessage());
                 log.info("Method name                 : {}", method.getName());
+                if(method.getName().equals("uploadFileUpdateSuccess") ||
+                        method.getName().equals("uploadFileUpdateParcelCode") ||
+                        method.getName().equals("uploadFileUpdateCancel")){
+                    String state = "";
+                    if(method.getName().equals("uploadFileUpdateSuccess")){
+                        state = "Success";
+                    }else if(method.getName().equals("uploadFileUpdateParcelCode")){
+                        state = "Shipping";
+                    }else if(method.getName().equals("uploadFileUpdateCancel")){
+                        state = "Cancel";
+                    }
+                    UpdateOrderEntity updateOrder = new UpdateOrderEntity();
+                    updateOrder.setStatus("failUploadFile");
+                    updateOrder.setState(state);
+                    updateOrder.setCreatedBy("system");
+                    updateOrder.setCreatedAt(new Date());
+                    updateOrderRepository.save(updateOrder);
+                }
                 for (Object param : params) {
                     log.info("Parameter value             : {}", param);
                 }
