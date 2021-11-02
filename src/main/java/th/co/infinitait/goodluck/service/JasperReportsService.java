53bd5877dd022649ec0111a8ac75678c845671d9
thanarat22 <Thanarat@nctthai.com>
2021-11-02 17:38:39 +0700

    .

--- src/main/java/th/co/infinitait/goodluck/service/JasperReportsService.java
+++ src/main/java/th/co/infinitait/goodluck/service/JasperReportsService.java
@@ -192,7 +192,7 @@ public class JasperReportsService {
 				generateReportPdf(jasperFileName, fileName, params);
 
 //				String exportPath = pdfFilePath + "/order_receipt_"+i+".pdf";
-				String exportPath = "/order_receipt_"+i+".pdf";
+				String exportPath = "order_receipt_"+i+".pdf";
 				log.info("downloadCustomerServiceCallReport exportPath1 : {}", exportPath);
 				File file = new File(exportPath);
 				ut.addSource(file);
@@ -200,7 +200,7 @@ public class JasperReportsService {
 				i++;
 			}
 //			ut.setDestinationFileName(pdfFilePath + "/order_receipt.pdf");
-			ut.setDestinationFileName("/order_receipt.pdf");
+			ut.setDestinationFileName("order_receipt.pdf");
 			ut.mergeDocuments();
 		}
 
